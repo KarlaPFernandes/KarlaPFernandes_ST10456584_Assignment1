24f428b70ccb7ba2075c2cd6aa5f32af3899f0a0
KarlaPFernandes <ST10456584@vcconnect.edu.za>
2024-03-19 15:35:51 +0200

    finish progect

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IceTask 1- Karla Patricia Fernandes"
+include(":app")
+ 
